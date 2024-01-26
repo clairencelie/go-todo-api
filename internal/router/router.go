cef9156cbcd379d53ccb0cb85c0a52fe1231928b
Clairence <clairencelie@gmail.com>
2024-01-26 22:01:33 +0700

    implement auth middleware to routes that need authentication

--- internal/router/router.go
+++ internal/router/router.go
@@ -13,17 +13,17 @@ func NewRouter(userController controller.UserController, todoController controll
 	router.POST("/api/login", authController.Login)
 	router.POST("/api/token/refresh", authController.RefreshToken)
 
-	router.POST("/api/user", userController.CreateUser)
-	router.GET("/api/user/:userId", userController.Get)
-	router.PUT("/api/user/:userId", userController.Update)
-	router.DELETE("/api/user/:userId", userController.Remove)
+	router.POST("/api/user", middleware.AuthMiddleware(userController.CreateUser))
+	router.GET("/api/user/:userId", middleware.AuthMiddleware(userController.Get))
+	router.PUT("/api/user/:userId", middleware.AuthMiddleware(userController.Update))
+	router.DELETE("/api/user/:userId", middleware.AuthMiddleware(userController.Remove))
 
-	router.POST("/api/todo", todoController.CreateTodo)
+	router.POST("/api/todo", middleware.AuthMiddleware(todoController.CreateTodo))
 	router.GET("/api/user/:userId/todo", middleware.AuthMiddleware(todoController.GetUserTodos))
-	router.GET("/api/todo/:todoId", todoController.Get)
-	router.PUT("/api/todo/:todoId", todoController.Update)
-	router.PATCH("/api/todo/completion/:todoId", todoController.UpdateTodoCompletion)
-	router.DELETE("/api/todo/:todoId", todoController.Remove)
+	router.GET("/api/todo/:todoId", middleware.AuthMiddleware(todoController.Get))
+	router.PUT("/api/todo/:todoId", middleware.AuthMiddleware(todoController.Update))
+	router.PATCH("/api/todo/completion/:todoId", middleware.AuthMiddleware(todoController.UpdateTodoCompletion))
+	router.DELETE("/api/todo/:todoId", middleware.AuthMiddleware(todoController.Remove))
 
 	return router
 }
